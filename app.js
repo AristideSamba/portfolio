8fa181aa7d2b56308dac9a0c4f1fab51e621522b
AristideSamba <sambaaristide@gmail.comm>
2024-06-03 05:35:33 +0100

    Derniers commits13

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const menuDeroulant = document.querySelector(".menu-icon")
 const navLinks = document.querySelector(".links")
+const overlay = document.querySelector('.overlay')
 
 menuDeroulant.addEventListener('click', ()=> {
     navLinks.classList.toggle('mobile-menu')
+    overlay.classList.toggle('active')
 })
