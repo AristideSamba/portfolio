a238aee49f2394cbcb3ddd781bf15dd97a2511d2
AristideSamba <sambaaristide@gmail.comm>
2024-07-03 19:17:33 +0100

    Update13

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const menuDeroulant = document.querySelector(".menu-icon")
-const navLinks = document.querySelector(".links")
-const overlay = document.querySelector('.overlay')
+const menuDeroulant = document.querySelector(".menu-icon");
+const iconeFermer = document.querySelector(".menu-burger-icon-fermer");
+const navLinks = document.querySelector(".links");
+const overlay = document.querySelector('.overlay');
 let prevBtn = document.getElementById('prev');
 let nextBtn = document.getElementById('next');
 let caroussel = document.querySelector('.caroussel');
@@ -47,6 +48,52 @@ prevBtn.onclick = () => {
   setSlider();
 }
 
-menuDeroulant.addEventListener('click', ()=> {
-  navLinks.classList.toggle('mobile-menu')
-})
+
+let menuOuvert = false; // Variable pour stocker l'état du menu (ouvert/fermé)
+let liensCliquables = true;
+
+menuDeroulant.addEventListener('click', () => {
+  basculerMenu();
+});
+
+iconeFermer.addEventListener('click', () => {
+  basculerMenu();
+});
+
+function basculerMenu() {
+  menuOuvert = !menuOuvert; // Inverser l'état du menu
+
+  if (menuOuvert) {
+    navLinks.classList.add('mobile-menu'); // Ouvrir le menu
+    iconeFermer.style.display = 'block'; // Afficher l'icône de fermeture
+    menuDeroulant.style.display = 'none'; // Masquer l'icône du menu burger
+    
+  } else {
+    navLinks.classList.remove('mobile-menu'); // Fermer le menu
+    iconeFermer.style.display = 'none'; // Masquer l'icône de fermeture
+    menuDeroulant.style.display = 'block'; // Afficher l'icône du menu burger
+  }
+}
+
+function showMenuIcon() {
+  if (window.matchMedia('(max-width: 1080px)').matches) {
+    menuOuvert = !menuOuvert;
+    menuDeroulant.style.display = 'block'; // Display the menu icon (only on small screens)
+    iconeFermer.style.display = 'none';
+  }
+}
+
+document.querySelectorAll('.nav-link').forEach(n => {
+  n.addEventListener('click', () => {
+    showMenuIcon(); // Show the menu icon (if appropriate)
+    navLinks.classList.remove('mobile-menu'); // Hide the mobile menu
+  });
+});
+
+// Hide hamburger menu on large screens (using media query)
+window.matchMedia('(min-width: 1080px)').addEventListener('change', (event) => {
+  if (event.matches) {
+    navLinks.classList.remove('mobile-menu'); // Hide the mobile menu on large screens
+  }
+});
+
